6f67a397fd72ca32c3a06493f5f92a26c7de0ad4
Jhurta952 <camachohurtado3@gmail.com>
2024-09-14 10:24:38 -0500

    Create trabajo2.js

---
+++ trabajo2.js
@@ -0,0 +1,24 @@
+
+// primer arreglo
+let num = ["Juan", "edi", "camilo"]; 
+
+console.log (num.length);
+
+//segunso arreglo
+let li = [100, 200, 300];
+
+console.log(li[2]); 
+console.log(li[1]); 
+console.log(li[0]); 
+
+//tercer arreglo
+num.push("santi", true); 
+console.log(num)
+
+//cuarto arreglo
+num.pop();
+console.log(num);
+
+//quinto arreglo
+li.pop(2);
+console.log(li);
